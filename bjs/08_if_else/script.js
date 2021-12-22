294c12922c9e7431a619f63bbeb456dc4c4a71bb
Oksana <preferenta@gmail.com>
2021-12-22 22:53:03 +0300

    2nd commit

--- bjs/08_if_else/script.js
+++ bjs/08_if_else/script.js
@@ -11,10 +11,24 @@ const answerField = document.getElementById('answerField');
 orderNumberField.innerText = orderNumber;
 answerField.innerText = `Вы загадали число ${answerNumber }?`;
 
-document.getElementById('btnRetry').addEventListener('click', function () {
-    minValue = 0;
-    maxValue = 100;
-    orderNumber = 0;
+document.getElementById('btnLess').addEventListener('click', function () {
+    if (gameRun){
+        if (minValue === maxValue){
+            const phraseRandom = Math.round( Math.random());
+            const answerPhrase = (phraseRandom === 1) ?
+                `Вы загадали неправильное число!\n\u{1F914}` :
+                `Я сдаюсь..\n\u{1F92F}`;
+
+            answerField.innerText = answerPhrase;
+            gameRun = false;
+        } else {
+            maxValue = answerNumber  + 1;
+            answerNumber  = Math.floor((minValue + maxValue) / 2);
+            orderNumber++;
+            orderNumberField.innerText = orderNumber;
+            answerField.innerText = `Вы загадали число ${answerNumber }?`;
+        }
+    }
 })
 
 document.getElementById('btnOver').addEventListener('click', function () {
@@ -44,3 +58,11 @@ document.getElementById('btnEqual').addEventListener('click', function () {
     }
 })
 
+document.getElementById('btnRetry').addEventListener('click', function () {
+    let minValue = parseInt(prompt('Минимальное знание числа для игры','0'));
+    let maxValue = parseInt(prompt('Максимальное знание числа для игры','100'));
+    alert(`Загадайте любое целое число от ${minValue} до ${maxValue}, а я его угадаю`);
+    let answerNumber  = Math.floor((minValue + maxValue) / 2);
+    let gameRun = true;
+    orderNumberField.innerText = 1;
+})
